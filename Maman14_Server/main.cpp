f6749b7ace158c4f77d1e39f592b9e36d10e0190
Stav Magalnik <ststav@gmail.com>
2021-08-28 18:47:38 +0300

    Create PACKET Request DATA CLASS

--- Maman14_Server/main.cpp
+++ Maman14_Server/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <thread>
+
+#define PORT 8080
 
 using namespace std;
 
-void main() {
+class PACKET {
+	unsigned int user_id;
+	char version;
+	char op;
+	unsigned short name_len;
+	char* filename;
+	unsigned int Payload;
+};
 
+void main() {
+	PACKET* p = new PACKET();
+	cout << "Size is: " << sizeof(p);
 
 }

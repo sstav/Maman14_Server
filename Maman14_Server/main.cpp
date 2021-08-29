5e4fb24ff100eaa8a1df8caac6b0c1b0056a4a35
mstav <mstav@technion.ac.il>
2021-08-29 15:02:13 +0300

    Set Delete and save files to server from client request.

--- Maman14_Server/main.cpp
+++ Maman14_Server/main.cpp
@@ -2,13 +2,23 @@
 #include <Windows.h>
 #include <thread>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <cstdio>
 #include <string>
+#include <vector>
 #include <regex>
+#include <random>
+#include <direct.h>
+#include "main.h"
 
 #pragma comment(lib,"ws2_32.lib")
 #pragma pack(1)
 
-#define PORT 8080
+#define getcwd _getcwd // stupid MSFT "deprecation" warning
+#define BACKUP_PATH ""
+#define RELATIVE_PATH "C:\\Users\\mstav\\source\\repos\\sstav\\Maman14_Server\\Debug\\"
+#define BACKUP_PATH2 "c:\\backupsvr\\"
 
 using namespace std;
 
@@ -22,6 +32,107 @@ public:
 	unsigned int size;
 	char* payload;
 };
+
+class IPSetting {
+	string ip;
+	string port;
+public:
+	IPSetting(string filename) {
+		ifstream  file(RELATIVE_PATH + filename);
+		stringstream buffer;
+		buffer << file.rdbuf();
+		string _data = this->trim(buffer.str());
+
+		int startPos = _data.find(":");
+		string set_ip = _data.substr(0, startPos);
+		string set_port = _data.substr(startPos+1, _data.length());
+
+		this->ip = set_ip;
+		this->port = set_port;
+	}
+
+	string getIp() {
+		return ip;
+	}
+
+	int getPort() {
+		if (port == "") return 0;
+		return stoi(port);
+	}
+
+	string trim(const string& str)
+	{
+		size_t first = str.find_first_not_of(' ');
+		if (string::npos == first)
+		{
+			return str;
+		}
+		size_t last = str.find_last_not_of(' ');
+		return str.substr(first, (last - first + 1));
+	}
+
+};
+
+
+class Backup {
+	vector<string> backup_files;
+public:
+	Backup(string filename) {
+		string line;
+		ifstream file;
+		file.open(RELATIVE_PATH + filename);
+
+		if (!file.is_open()) {
+			perror("Error open");
+			exit(EXIT_FAILURE);
+		}
+		while (getline(file, line)) {
+			backup_files.push_back(line);
+		}
+	}
+
+	int length() {
+		return this->getFiles().size();
+	}
+
+	vector<string> getFiles() {
+		return backup_files;
+	}
+
+};
+
+class ServerAction {
+	
+public:
+	static bool save_file_from_backup(string filename , string username, string file_data) {
+		try {
+			string str = (RELATIVE_PATH + username);
+			try {
+				_mkdir((RELATIVE_PATH + username).c_str());
+			}
+			catch (...) {}
+				
+			
+			ofstream out(RELATIVE_PATH + username + "\\"  + filename);
+			out << file_data.c_str();
+			out.close();
+			return true;
+		}
+		catch (exception& es) {
+			cout << "ERROR: " << es.what();
+			return false;
+		}
+	}
+
+	static bool delete_file(string filename, string username) {
+		if (remove((RELATIVE_PATH + username + "\\" + filename).c_str()) != 0)
+			perror("Error deleting file");
+		else
+			puts("File successfully deleted");
+		return 0;
+	}
+};
+
 
 string string_replace(string find, string replace , string str , int limit = 0) {
 	int start_pos,i=0;
@@ -41,6 +152,40 @@ string string_replace(string find, string replace , string str , int limit = 0) 
 	return str;
 
 }
+
+std::vector<std::string> split(const std::string& str, char delim) {
+	std::vector<std::string> strings;
+	size_t start;
+	size_t end = 0;
+	while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+		end = str.find(delim, start);
+		strings.push_back(str.substr(start, end - start));
+	}
+	return strings;
+}
+
+
+string getModuleFilePath(char* argv) {
+	std::string cur_dir(argv);
+	int pos = cur_dir.find_last_of("/\\");
+
+	std::cout << "path: " << cur_dir.substr(0, pos) << std::endl;
+	std::cout << "file: " << cur_dir.substr(pos + 1) << std::endl;
+	return 0;
+}
+
+bool IsDirectory(const char* path)
+{
+	struct stat info;
+
+	if (stat(path, &info) != 0)
+		return false;
+	else if (info.st_mode & S_IFDIR)
+		return true;
+	else
+		return false;
+}
+
 
 void HandleRequest(SOCKET client_incomming) {
 	cout << "Detected: Incomming Request..." << endl;
@@ -70,15 +215,30 @@ void HandleClient(SOCKET s) {
 }
 
 
+IPSetting* init_IPSetting() {
+	IPSetting* ipsetting = new IPSetting("server.info");
+	cout << ipsetting->getIp() << " : " << ipsetting->getPort();
+	return ipsetting;
+}
+
+Backup* init_Backup() {
+	Backup* backup_files = new Backup("backup.info");
+	return backup_files;
+}
+
+int create_key() {
+	return (int)rand();
+}
 void main() {
-	PACKET* p = new PACKET();
-	p->user_id = 10;
-	p->version = '1';
-	p->op = '2';
-	p->name_len = 3;
-	p->filename = nullptr;
-	p->size = 4;
-	p->payload = nullptr;
+
+	IPSetting* ipsetting = init_IPSetting();
+	Backup* backup_files = init_Backup();
+	for (int i = 0; i < backup_files->length(); i++) {
+		cout << backup_files->getFiles()[i];
+	}
+
+	ServerAction::save_file_from_backup("sss.txt", "isma", "Hellow Hellow Hello shalom\r\nBatia Save");
+	ServerAction::delete_file("fgfg.txt", "isma");
 
 
 	cout << "Server is running..." << endl;
@@ -89,13 +249,13 @@ void main() {
 	struct sockaddr_in server;
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(PORT);
+	server.sin_port = htons(ipsetting->getPort());
 	//Bing the socket to the ip/port
 	bind(s, (const sockaddr*)&server, sizeof(server));
 	//Listen for incoming requests
 	//SOMAXCONN  = 0x7fffffff -> Means unlimited connectation request is allowed.
 	listen(s, SOMAXCONN);
-	cout << "Listening on 127.0.0.1:" << PORT << endl;
+	cout << "Listening on " << ipsetting->getIp() << ":" << ipsetting->getPort() << endl;
 	cout << "waiting for requests..." << endl;
 	while (true) {
 		HandleClient(s);
